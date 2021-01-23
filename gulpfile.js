d20a39548cb4e3e6c273cd471f0ece20d5b49471
Yharooer <me@bentleycarr.com>
2021-01-23 12:35:21 +0000

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
+const sourcemaps = require("gulp-sourcemaps");
+const rename = require('gulp-rename');
+
+gulp.task('default', () => 
+    browserify({
+        basedir: '.',
+        paths: ['src'],
+        debug: true,
+        entries: ['src/main.js'],
+        cache: {},
+        packageCache: {}
+    })
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./bin'))
+);
