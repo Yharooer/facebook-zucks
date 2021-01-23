076248320c6b70a4dfd954128f56701a610c45d8
Jakub Priban <jp775@cam.ac.uk>
2021-01-23 16:21:04 +0000

    parse header and footer

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,7 @@ class Post {
     constructor(content) {
         const content_depth = 10;
         const main_content = this.getNestedChildren(content, content_depth).children[1].children[0];
+        this.root = main_content;
         this.header = this.parseHeader(main_content.children[1]);
         this.middle = this.parseMiddle(main_content.children[2]);
         this.footer = this.parseFooter(main_content.children[3]);
@@ -10,27 +11,33 @@ class Post {
     getNestedChildren(dom, n) {
         let result = dom;
         for (let i = 0; i < n; i++) {
-            result = result.children[0];
+            result = result?.children[0];
         }
         return result;
     }
 
-    parseHeader(dom) {
-        let indices = [0, 1, 0, 0, 0];
-        let result = dom;
-        for (let i of indices) {
-            result = result.children[indices[i]];
-        }
-        console.log(result);
-        return result;
+    parseHeader(root) {
+        let image = root.children[0]?.children[0]?.querySelector('a svg image');
+        let title = root.children[0]?.children[1]?.querySelector('h4 a span');
+        // TODO: this doesn't work for posts to groups: eg. Jakub Priban > JCSU Group
+        // will only select 'Jakub Priban' and not 'JCSU Group'
+        return { root, image, title };
     }
 
-    parseMiddle(dom) {
+    parseMiddle(root) {
 
     }
 
-    parseFooter(dom) {
-
+    parseFooter(root) {
+        let thing = this.getNestedChildren(root, 6);
+        const likes = thing?.children[0]?.children[1]?.querySelector('div span span span span');
+        const comments_shares = thing?.children[thing.children.length - 1];
+        const comments = comments_shares?.children[0]?.querySelector('div span[dir="auto"]');
+        const shares = comments_shares?.children[1]?.querySelector('div span[dir="auto"]');
+        const buttons = this.getNestedChildren(root, 4)?.children[1]?.children[0]?.children;
+        return {
+            likes, comments, shares,
+            buttons: buttons ? Array.from(buttons) : null };
     }
 }
 
