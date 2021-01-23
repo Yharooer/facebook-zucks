5b0566fb88a016d2fcada703ca9203715dd6ffd1
Jakub Priban <jp775@cam.ac.uk>
2021-01-23 13:58:29 +0000

    basic scroll detection

--- src/FeedParser.js
+++ src/FeedParser.js
@@ -5,11 +5,12 @@ class FeedParser {
     }
 
     parseFeed() {
-        this.parseWholeFeed();
+        return this.parseWholeFeed();
     }
 
     parseWholeFeed() {
         const posts = Array.from(document.querySelectorAll('[role=feed]')[0].children).filter(e => e.hasAttribute('data-pagelet'));
+        return posts;
     }
 
 }
--- src/Program.js
+++ src/Program.js
@@ -1,15 +1,36 @@
+import FeedParser from './FeedParser'
+
+
 class FacebookZucksProgram {
 
     constructor() {
-
+        this.parser = new FeedParser();
+        this.feed = [];
     }
 
     initialise() {
-        console.log('TEST TEST TEST TEST 2');
+        console.log('Initializing FacebookZucksProgram');
+
+        document.addEventListener('scroll', (e) => {
+            if (!ticking) { // this garbage is to throttle the rate at which onScroll is called
+                window.requestAnimationFrame(() => {
+                    this.onScroll();
+                    ticking = false;
+                });
+                ticking = true;
+            }
+        });
+    }
+
+    onScroll() {
+        const newFeed = this.parser.parseFeed();
+        if (newFeed.length > this.feed.length) {
+            this.feed = newFeed;
+        }
     }
 
     start () {
-        console.log('STARTED');
+        console.log('Starting FacebookZucksProgram');
     }
 
 }
